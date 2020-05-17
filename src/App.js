fea88c8a6c29f660f2121e6c6f27e58d0c2e2686
PabloFNK <pablo.fnk.foncas@gmail.com>
2020-05-17 10:43:36 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState } from "react";
+import backendUrl from "./services/urls";
 
 function App() {
+  const [url, setUrl] = useState("");
+
+  const download = () => {
+    fetch(`${backendUrl}/download?url=${url}`)
+      .then(async (res) => {
+        if (!res.ok) {
+          const { errorMessage } = await res.json();
+          throw new Error(errorMessage);
+        } else {
+          return await res.blob();
+        }
+      })
+      .then((res) => {
+        console.log(res);
+        var url = window.URL.createObjectURL(res);
+        var a = document.createElement("a");
+        a.href = url;
+        a.download = "myvideo.mp4";
+        document.body.appendChild(a); // we need to append the element to the dom -> otherwise it will not work in firefox
+        a.click();
+        a.remove(); //afterwards we remove the element again
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <label htmlFor="downloadInput">Introduce youtube URL:</label>
+      <input id="downloadInput" onChange={(e) => setUrl(e.target.value)} />
+      <button onClick={download}>Download video</button>
+    </Fragment>
   );
 }
 
